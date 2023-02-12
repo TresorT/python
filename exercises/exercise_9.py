6ff32f9ddb2ca4e7595178e6ba287b2f2fd31a7b
tresortwendimbadi <tresor.twendi@gmail.com>
2023-02-12 13:30:19 +0000

    Exercise 1: Updated exercises.

--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,28 +1,24 @@
 """
-
-In this exercise, we will continue working on the ‘Bad Ambiance!’ app.
-Along with being able to play the game in story mode, completing missions, it is also possible to
-play a couple of silly side games such as ‘Hyper Hurdles’ and ‘We Have Lift-off!’.
-Although these may seem like silly side games, a game player can actually unlock weapons and
-missions by achieving certain milestones in these side games. For instance, if the Hyper Hurdles
-game is completed in 10 seconds or under, or the Car rises off the ground by more than 11
-centimetres when going over a hill at speed, a new weapon is unlocked in story mode. If both of the
-above criteria has been met (hurdles in 10 seconds or under and height is over 11 centimetres), a
-new mission is unlocked.
-You have been given the following sample data:
-  hurdle_time = 9.95
-  height_from_road = 10.75
-
-You have been tasked with writing two expressions to check if the game player has managed to
-unlock weapons and/or missions.
- The expression to determine if both criteria has been met, should store the result in a
-variable called unlock_mission
- The expression to determine if either of the criteria has been met, should be stored in a
-variable called unlock_weapon
+Exercise 7
+You were tasked with writing two expressions that successfully check whether:
+a) A gamer has unlocked a new mission
+b) A gamer has unlocked a new weapon
+You were given the following sample data to work with:
 
 """
 hurdle_time = 9.95
 height_from_road = 10.75
 
-unlock_mission = hurdle_time and height_from_road
-unlock_weapon = hurdle_time or height_from_road
+"""
+The following criteria was given for the silly side games:
+a) Hyper Hurdles should be completed in 10 seconds or under
+b) The Car in We Have Lift-off should rise off the ground by more than 11 centimetres
+If both criteria are met, a new mission is unlocked. 
+The following statement will check if both criteria has been met:
+"""
+unlock_mission = hurdle_time <= 10 and height_from_road > 11
+
+# If either criteria are met, a new weapon is unlocked.
+# The following statement will check if either of the criteria has been met:
+
+unlock_weapon = hurdle_time <= 10 or height_from_road > 11
